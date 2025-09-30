e3e74b454a4327917a9fef993aac94ded35165c5
arthurgrassi08 <145673458+arthurgrassi08@users.noreply.github.com>
2025-09-30 12:41:29 +0000

    alteração

--- script.js
+++ script.js
@@ -71,5 +71,11 @@ function classificaSenha(tamanhoAlfabeto){
     forcaSenha.classList.remove('fraca','media','forte');
     if (entropia > 57){
         forcaSenha.classList.add('forte');
-    } else if(entropia > 35 && entropia < 57)
+    } else if (entropia > 35 && entropia < 57 ) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35){
+        forcaSenha.classList.add('fraca');
+    }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir essa senha.";
 }
